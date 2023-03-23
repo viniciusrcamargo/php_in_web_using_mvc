c194ee9bbe89d41bdd920d37246162fd362b3e0b
Vinicius Rodrigues <viniciusr.camargo@gmail.com>
2023-03-23 18:24:09 -0300

    creating new controllers

---
+++ src/controller/VideoCreateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Repository\VideoRepository;
+use PDO;
+
+class VideoCreateController
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+
+    }
+
+    public function processaRequisicao()
+    {
+
+    }
+
+}
